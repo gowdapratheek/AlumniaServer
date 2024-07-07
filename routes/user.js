a2f74375dacd1bd0657f52abfdf0b8ddbff69a09
Master_shiv.info.me <103109048+shivamkumarsha@users.noreply.github.com>
2024-07-07 15:41:49 +0530

    Backend done

---
+++ routes/user.js
@@ -0,0 +1,25 @@
+import express from "express";
+const router = express.Router();
+import {
+  getAllUsers,
+  register,
+  sendRegisterOTP,
+  sendForgetPasswordOTP,
+  changePassword,
+  login,
+  deleteAllUsers,
+} from "../controllers/user.js";
+
+router.get("/get-all-users", getAllUsers);
+
+router.post("/send-register-otp", sendRegisterOTP);
+router.post("/register", register);
+
+router.put("/login", login);
+
+router.post("/send-forget-pass-otp", sendForgetPasswordOTP);
+router.put("/change-password", changePassword);
+
+router.delete("/delete_all_users", deleteAllUsers);
+
+export default router;
